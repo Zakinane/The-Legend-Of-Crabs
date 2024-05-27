8c293b6d96a15a4b9a3355dbe105dccda574ad26
Zak <djabazaki@gmail.com>
2024-05-27 02:14:12 +0100

    Started doing da things

---
+++ game/game.js
@@ -0,0 +1,20 @@
+//AUDIO
+document.addEventListener("DOMContentLoaded", function () {
+  var BoutonAudio = document.querySelector(".SoundButton");
+  var audio = document.querySelector("audio");
+
+  BoutonAudio.addEventListener("click", function () {
+    if (audio.paused) {
+      BoutonAudio.setAttribute("src", "../img/SoundON.png");
+      audio.play();
+    } else {
+      BoutonAudio.setAttribute("src", "../img/SoundOFF.png");
+      audio.pause();
+    }
+  });
+
+  audio.addEventListener("ended", function () {
+    ReplayIntro.removeAttribute("hidden");
+  });
+});
+//CONTROLS
