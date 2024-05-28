2594423239848ec70aba33889141b1d1d5fc2eb1
Zak <djabazaki@gmail.com>
2024-05-28 23:54:44 +0100

    Un enemy ca marche

--- game/game.js
+++ game/game.js
@@ -1,5 +1,5 @@
 ////////////////////////////////SCREEN
-const screen = document.querySelector(".screen");
+const screen = document.querySelector("main");
 function getScreenBounds() {
   const rect = screen.getBoundingClientRect();
   return { left: rect.left, right: rect.right };
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 //others
 const pioupiou = document.querySelector(".pioupiou");
+const boumEnemy = document.querySelector(".boumEnemy")
 
 ///////////////////////////////////////////////ME
 const You = document.querySelector(".You");
@@ -40,7 +41,37 @@ You.addEventListener("click", function () {
   audioMoney.play();
 });
 
-/////////////////////////////////////////////ACTIONS
+
+/////////////////////////////////////////////ENEMY
+const enemyTest = document.querySelector(".enemyTest")
+// const enemies = [];
+
+// function createEnemy() {
+//   const enemy = document.createElement("img");
+//   enemy.classList.add("enemy");
+//   enemy.src = "../img/plankton.png";
+//   enemy.style.left = You.style.left;
+//   enemies.push(enemy);
+//   screen.appendChild(enemy);
+
+//   setTimeout(createEnemy,1000);
+// }
+// setTimeout(createEnemy,2000);
+
+function deathEnemy(enemyTest, ProjX, ProjY, ProjW) {
+  if (
+    ProjY <= enemyTest.y + enemyTest.height &&
+    ProjX >= enemyTest.x - enemyTest.width &&
+    ProjX + ProjW <= enemyTest.x + enemyTest.width
+  ) {
+    boumEnemy.play();
+    //// enemy.remove();
+    enemyTest.remove();
+    return 1;
+  }
+}
+
+/////////////////////////////////////////////ACTIONS  
 function goLeft() {
   let Xpos = getXpos();
   const { left: MAX_LEFT } = getScreenBounds();
@@ -57,13 +88,41 @@ function goRight() {
   }
 }
 
-function Shoot() {
-  let Xpos = getXpos();
+function ShootUp() {
   const projectile = document.createElement("img");
   projectile.src = "../img/moneyyyy.png";
   projectile.classList.add("projectile");
+  screen.appendChild(projectile);
+  projectile.style.left = You.style.left;
+  projectile.style.bottom = "60px";
   pioupiou.play();
+
+  function moveProjectile() {
+    let currentBottom = parseInt(projectile.style.bottom);
+    projectile.style.bottom = currentBottom + 10 + "px";
+
+    //colision all enemies
+
+    // enemies.forEach((enemy) => {
+    //   deathEnemy(enemy,projectile.x,projectile.y,projectile.width);
+    // });
+    if(deathEnemy(enemyTest,projectile.x,projectile.y,projectile.width)){
+      projectile.remove();
+    }
+    
+
+    //out of screen
+    if (currentBottom < 800) {
+      requestAnimationFrame(moveProjectile);
+    } else {
+      projectile.remove();
+    }
+  }
+
+  // Lancer le déplacement du projectile
+  requestAnimationFrame(moveProjectile);
 }
+
 
 //////////////////////////////////////////CONTROLS
 //Fluidity
@@ -76,9 +135,17 @@ document.addEventListener("keyup", (key) => {
   keys[key.code] = false;
 });
 
+let lastShotTime = 0;
 function Controls() {
   if (keys["Space"]) {
-    Shoot();
+    const currentTime = Date.now();
+    const timeSinceLastShot = currentTime - lastShotTime;
+
+    // if (timeSinceLastShot > 500) { //latence
+    //   ShootUp();
+    //   lastShotTime = currentTime;
+    // }
+    ShootUp(); //A SUP
   }
   if (keys["ArrowLeft"]) {
     goLeft();
