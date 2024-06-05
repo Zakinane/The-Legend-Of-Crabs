d111c0e589602359eeac32d7a3d5dff795a3043f
Zak <djabazaki@gmail.com>
2024-06-05 05:46:50 +0100

    Share buttons

--- app.js
+++ app.js
@@ -42,3 +42,15 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 16000);
   
 });
+/////SHARE
+function shareScoreFacebook() {
+  const gameUrl = `https://zakinane.github.io/The-Legend-Of-Crabs/`;
+  const quote = `I scored ${localStorage.getItem("game")} at this game!`;
+  const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(gameUrl)}&quote=${encodeURIComponent(quote)}`;
+  window.open(url, '_blank');
+}
+function shareScoreX() {
+  const text = `I scored ${localStorage.getItem("game")} at this game! https://zakinane.github.io/The-Legend-Of-Crabs/`;
+  const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}`;
+  window.open(url, '_blank');
+}
--- game/game.js
+++ game/game.js
@@ -39,6 +39,20 @@ const gameover = document.querySelector(".gameover");
 
 let gameOverSoundPlayed = false;
 
+///////////////////////////////////////////////////////////////////////////////////////////////////SHARE
+
+function shareScoreFacebook() {
+  const gameUrl = `https://zakinane.github.io/The-Legend-Of-Crabs/`;
+  const quote = `I scored ${localStorage.getItem("game")} at this game!`;
+  const url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(gameUrl)}&quote=${encodeURIComponent(quote)}`;
+  window.open(url, '_blank');
+}
+function shareScoreX() {
+  const text = `I scored ${localStorage.getItem("game")} at this game! https://zakinane.github.io/The-Legend-Of-Crabs/`;
+  const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}`;
+  window.open(url, '_blank');
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////SCORE DATA
 function saveData(highScore) {
   localStorage.setItem("game", highScore);
